4d928dcfa7226fca92d663b514302d3d092eb4ab
Layle <luca@marcelli.ch>
2021-04-29 17:41:15 +0200

    lol

--- FallGuysHooks/dllmain.cpp
+++ FallGuysHooks/dllmain.cpp
@@ -5,33 +5,15 @@
 #include <easyhook.h>
 #include "hooks.hpp"
 
-extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO * remote_info);
+using namespace hook::types;
 
-bool hook(void* original, void* hook)
-{
-	HOOK_TRACE_INFO hHook = { NULL };
-	NTSTATUS result = LhInstallHook(
-		original,
-		hook,
-		NULL,
-		&hHook);
-	if (FAILED(result))
-	{
-		std::wstring error = RtlGetLastErrorString();
-		return false;
-	}
-	else
-	{
-		ULONG ACLEntries[1] = { 0 };
-		LhSetExclusiveACL(ACLEntries, 1, &hHook);
-		return true;
-	}
-}
+extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO * remote_info);
 
 void allocate_console()
 {
 	winapi::process::alloc_console();
-	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+	//freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+	freopen("debug.log", "w", stdout);
 }
 
 void* find_module(std::wstring target_module)
@@ -54,126 +36,6 @@ void* find_module(std::wstring target_module)
 	return nullptr;
 }
 
-void hook_pnt_set_username(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] PartyNameTag::SetUsername @ " << ptr << std::endl;
-	hooks::pnt::original_set_username = reinterpret_cast<
-		hooks::types::pnt_set_username_t>(ptr);
-	hook(ptr, hooks::pnt::set_username_hook);
-}
-
-void hook_pnt_set_nickname(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] PartyNameTag::SetNickname @ " << ptr << std::endl;
-	hooks::pnt::original_set_nickname = reinterpret_cast<
-		hooks::types::pnt_set_nickname_t>(ptr);
-	hook(ptr, hooks::pnt::set_nickname_hook);
-}
-
-void hook_pp_set_displayname(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] PlayerProfile::SetDisplayname @ " << ptr << std::endl;
-	hooks::pp::original_set_displayname = reinterpret_cast<
-		hooks::types::pp_set_displayname_t>(ptr);
-	hook(ptr, hooks::pp::set_displayname_hook);
-}
-
-void hook3(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] ClientGameManager::GetDisplayNameForNetObjID @ " << ptr << std::endl;
-	hooks::cgm::original_get_displayname = reinterpret_cast<
-		hooks::types::cgm_get_displayname_for_net_obj_id_t>(ptr);
-	hook(ptr, hooks::cgm::get_displayname_hook);
-}
-
-void hook4(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] MPGNetObjectManager::FixedUpdateAll @ " << ptr << std::endl;
-	hooks::obj_manager::original_fixed_update_all = reinterpret_cast<
-		hooks::types::obj_manager_fixed_update_all>(ptr);
-	hook(ptr, hooks::obj_manager::fixed_update_all_hook);
-}
-
-void hook5(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] Debug::Log @ " << ptr << std::endl;
-	hooks::dbg::original_log = reinterpret_cast<
-		hooks::types::dbg_log_t>(ptr);
-	hook(ptr, hooks::dbg::log_hook);
-}
-
-void hook6(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] ClientGameManager::DebugMakePlayerFast @ " << ptr << std::endl;
-	hooks::cgm::original_debug_make_player_fast = reinterpret_cast<
-		hooks::types::cgm_debug_make_player_fast_t>(ptr);
-	hook(ptr, hooks::cgm::debug_make_player_fast_hook);
-}
-
-void hook7(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] ClientGameManager::Update @ " << ptr << std::endl;
-	hooks::cgm::original_update = reinterpret_cast<
-		hooks::types::cgm_update_t>(ptr);
-	hook(ptr, hooks::cgm::update_hook);
-}
-
-void hook8(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] ClientGameManager::GetMyPlayer @ " << ptr << std::endl;
-	hooks::cgm::original_get_my_player = reinterpret_cast<
-		hooks::types::cgm_get_my_player_t>(ptr);
-	hook(ptr, hooks::cgm::get_my_player_hook);
-}
-
-void hook9(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] ClientGameManager::DebugSetZeroGravity @ " << ptr << std::endl;
-	hooks::fgcc::original_debug_set_zero_gravity = reinterpret_cast<
-		hooks::types::fgcc_debug_set_zero_gravity_t>(ptr);
-	hook(ptr, hooks::fgcc::debug_set_zero_gravity_hook);
-}
-
-void hook10(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] FallGuysCharacterController::Stun @ " << ptr << std::endl;
-	hooks::fgcc::original_stun = reinterpret_cast<
-		hooks::types::fgcc_stun_t>(ptr);
-	hook(ptr, hooks::fgcc::stun_hook);
-}
-
-void hook11(void* game_assembly, int offset)
-{
-	auto addr = reinterpret_cast<uint64_t>(game_assembly) + offset;
-	auto ptr = reinterpret_cast<void*>(addr);
-	std::cout << "[HOOKS] FallGuysCharacterController::GetCanJump @ " << ptr << std::endl;
-	hooks::fgcc::original_get_can_jump = reinterpret_cast<
-		hooks::types::fgcc_get_can_jump_t>(ptr);
-	hook(ptr, hooks::fgcc::get_can_jump_hook);
-}
-
 void __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* remote_info)
 {
 	allocate_console();
@@ -181,16 +43,16 @@ void __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* remote_info)
 	auto game_assembly = find_module(L"GameAssembly.dll");
 	std::cout << "[HOOKS] GameAssembly.dll @ " << game_assembly << std::endl;
 	
-	hook_pnt_set_username(game_assembly, 0x175bd80);
-	hook_pnt_set_nickname(game_assembly, 0x175baa0);
-	hook_pp_set_displayname(game_assembly, 0x17d67d0);
-	hook3(game_assembly, 0x1742220);
-	hook4(game_assembly, 0x7132a0);
-	hook5(game_assembly, 0x1b4b360);
-	hook6(game_assembly, 0x1740cf0);
-	hook7(game_assembly, 0x17483f0);
-	hook8(game_assembly, 0x1742400);
-	hook9(game_assembly, 0x1798720);
-	hook10(game_assembly, 0x1796030);
-	hook11(game_assembly, 0x17978f0);
+	hooks::add_hook<set_username_t>(game_assembly, 0x175bd80, hooks::set_username_hook, "PartyNameTag::SetUsername");
+	hooks::add_hook<set_nickname_t>(game_assembly, 0x175baa0, hooks::set_nickname_hook, "PartyNameTag::SetNickname");
+	hooks::add_hook<set_displayname_t>(game_assembly, 0x17d67d0, hooks::set_displayname_hook, "PlayerProfile::SetDisplayname");
+	hooks::add_hook<get_displayname_t>(game_assembly, 0x1742220, hooks::get_displayname_hook, "ClientGameManager::GetDisplayNameForNetObjID");
+	hooks::add_hook<fixed_update_all_t>(game_assembly, 0x07132a0, hooks::fixed_update_all_hook, "MPGNetObjectManager::FixedUpdateAll");
+	hooks::add_hook<log_t>(game_assembly, 0x1b4b360, hooks::log_hook, "Debug::Log");
+	hooks::add_hook<debug_make_player_fast_t>(game_assembly, 0x1740cf0, hooks::debug_make_player_fast_hook, "ClientGameManager::DebugMakePlayerFast");
+	hooks::add_hook<update_t>(game_assembly, 0x1742400, hooks::update_hook, "ClientGameManager::Update");
+	hooks::add_hook<get_my_player_t>(game_assembly, 0x1742400, hooks::get_my_player_hook, "ClientGameManager::GetMyPlayer");
+	hooks::add_hook<debug_set_zero_gravity_t>(game_assembly, 0x1742400, hooks::debug_set_zero_gravity_hook, "ClientGameManager::DebugSetZeroGravity");
+	hooks::add_hook<stun_t>(game_assembly, 0x1796030, hooks::stun_hook, "FallGuysCharacterController::Stun");
+	hooks::add_hook<get_can_jump_t>(game_assembly, 0x17978f0, hooks::get_can_jump_hook, "FallGuysCharacterController::GetCanJump");
 }
--- FallGuysHooks/hooks.hpp
+++ FallGuysHooks/hooks.hpp
@@ -6,166 +6,247 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <map>
+#include <any>
+#include <type_traits>
+#include <utility>
+#include <mutex>
+#include <easyhook.h>
 #include "wrapper.hpp"
 
-namespace hooks
+namespace hook::types
 {
-    class types
+    struct string
     {
-    public:
-        struct string
-        {
-            void* self;
-            uint64_t idk0;
-            uint32_t length;
-            wchar_t value[0];
-        };
-        struct net_obj
-        {
-            uint32_t network_id;
-        };
-        using pnt_set_username_t = int(__fastcall*)(void* self, string* username);
-        using pnt_set_nickname_t = int(__fastcall*)(void* self, string* nickname);
-        using pp_set_displayname_t = int(__fastcall*)(void* self, string* displayname);
-        using cgm_get_displayname_for_net_obj_id_t = int(__fastcall*)(void* self, net_obj* obj);
-        using cgm_debug_make_player_fast_t = int(__fastcall*)(void* self);
-        using cgm_update_t = int(__fastcall*)(void* self);
-        using cgm_get_my_player_t = void*(__fastcall*)(void* self);
-        using obj_manager_fixed_update_all = int(__fastcall*)(void* self);
-        using dbg_log_t = void(__stdcall*)(string* message, void* method);
-        using fgcc_stun_t = int(__fastcall*)(void* self);
-        using fgcc_get_can_jump_t = bool(__fastcall*)(void* self);
-        using fgcc_debug_set_zero_gravity_t = void(__fastcall*)(void* self, bool enabled);
+        void* self;
+        uint64_t idk0;
+        uint32_t length;
+        wchar_t value[0];
+    };
+    struct net_obj
+    {
+        uint32_t network_id;
     };
 
-    class pnt
+    using set_username_t =  int(__fastcall*)(void* self, string* username);
+    using set_nickname_t = int(__fastcall*)(void* self, string* nickname);
+    using set_displayname_t = int(__fastcall*)(void* self, string* displayname);
+    using get_displayname_t = int(__fastcall*)(void* self, net_obj* obj);
+    using debug_make_player_fast_t = void*(__fastcall*)(void* self);
+    using update_t = void*(__fastcall*)(void* self);
+    using get_my_player_t = void*(__fastcall*)(void* self);
+    using fixed_update_all_t = int(__fastcall*)(void* self);
+    using log_t = void(__stdcall*)(string* message, void* method);
+    using stun_t = int(__fastcall*)(void* self);
+    using get_can_jump_t = bool(__fastcall*)(void* self);
+    using debug_set_zero_gravity_t = void(__fastcall*)(void* self, bool enabled);
+}
+
+using namespace hook::types;
+
+class hooks
+{
+private:
+    static inline std::unordered_map<void*, std::pair<std::string, void*>> m_hooks = {};
+
+    template<class T = void>
+    static void log(std::vector<std::any>& args, std::string& name, std::any ret = nullptr)
     {
-    public:
-        static inline types::pnt_set_username_t original_set_username = nullptr;
-        static int __fastcall set_username_hook(
-            void* self, types::string* username)
+        std::cout << name << "(";
+        for (auto i = 0; i < args.size(); ++i)
         {
-            username->value[0] = 'B';
-            
-            wprintf(L"SetUsername(%p, %ls)\n", self, username->value);
-            return hooks::pnt::original_set_username(self, username);
-        }
+            auto& arg = args[i];
 
-        static inline types::pnt_set_nickname_t original_set_nickname = nullptr;
-        static int __fastcall set_nickname_hook(
-            void* self, types::string* nickname)
-        {
-            nickname->value[0] = 'B';
-            
-            wprintf(L"SetNickname(%p, %ls)\n", self, nickname->value);
-            return hooks::pnt::original_set_nickname(self, nickname);
-        }
-    };
-
-    class pp
-    {
-    public:
-        static inline types::pp_set_displayname_t original_set_displayname = nullptr;
-        static int __fastcall set_displayname_hook(
-            void* self, types::string* displayname)
-        {
-            displayname->value[0] = 'B';
-
-            wprintf(L"SetDisplayname(%p, %ls)\n", self, displayname->value);
-            return hooks::pp::original_set_displayname(self, displayname);
-        }
-    };
-
-    class cgm
-    {
-    public:
-        static inline types::cgm_get_displayname_for_net_obj_id_t original_get_displayname = nullptr;
-        static int __fastcall get_displayname_hook(void* self, types::net_obj* netobj)
-        {
-            printf("GetDisplayname(%p, %d)\n", self, netobj->network_id);
-            return hooks::cgm::original_get_displayname(self, netobj);
-        }
-
-        static inline types::cgm_debug_make_player_fast_t original_debug_make_player_fast = nullptr;
-        static int __fastcall debug_make_player_fast_hook(void* self)
-        {
-            printf("DebugMakePlayerFast(%p)\n", self);
-            return hooks::cgm::original_debug_make_player_fast(self);
-        }
-
-        static inline bool hacked = false;
-
-        static inline types::cgm_update_t original_update = nullptr;
-        static int __fastcall update_hook(void* self)
-        {
-            //printf("Update(%p)\n");
-            if (!hacked)
+            if (arg.type() == typeid(string*))
             {
-                if (original_get_my_player(self) != nullptr)
-                {
-                    original_debug_make_player_fast(self);
-                    //hooks::fgcc::original_debug_set_zero_gravity(self, true);
-                    hacked = true;
-                }
+                wprintf(L"%ls", std::any_cast<string*>(arg)->value);
+                //std::wcout << std::any_cast<string*>(arg)->value;
+            }
+            else if (arg.type() == typeid(void*))
+            {
+                std::cout << std::any_cast<void*>(arg);
+            }
+            else
+            {
+                std::cout << std::any_cast<int>(arg);
             }
 
-            return hooks::cgm::original_update(self);
+            if (i != args.size() - 1)
+            {
+                std::cout << ", ";
+            }
         }
 
-        static inline types::cgm_get_my_player_t original_get_my_player = nullptr;
-        static void* __fastcall get_my_player_hook(void* self)
+        std::cout << ")";
+
+        if (!std::is_same<T, void>::value)
         {
-            void* ret = original_get_my_player(self);
-            if(ret != nullptr)
-                printf("GetMyPlayer(%p) -> %p\n", self, ret);
-            return ret;
-        }
-    };
+            std::cout << " -> ";
 
-    class obj_manager
+            if (ret.type() == typeid(void*))
+            {
+                std::cout << std::any_cast<void*>(ret);
+            }
+            else
+            {
+                std::cout << std::any_cast<int>(ret);
+            }
+        }
+
+        std::cout << std::endl;
+    }
+
+    template<class T>
+    static T get_original(void* hook)
     {
-    public:
-        static inline types::obj_manager_fixed_update_all original_fixed_update_all = nullptr;
-        static int __fastcall fixed_update_all_hook(void* self)
-        {
-            printf("FixedUpdateAll(%p)\n", self);
-            return hooks::obj_manager::original_fixed_update_all(self);
-        }
-    };
+        return reinterpret_cast<T>(m_hooks[hook].second);
+    }
 
-    class dbg
+    template<class T, class U, typename... Args>
+    static T call_original(void* hook, Args&&... args)
     {
-    public:
-        static inline types::dbg_log_t original_log = nullptr;
-        static void __stdcall log_hook(types::string* message, void* method)
-        {
-            //wprintf(L"Log(%ls, %p)\n", message->value, method);
-            return original_log(message, method);
-        }
-    };
+        auto& [name, ptr] = m_hooks[hook];
+        auto fn = reinterpret_cast<U>(ptr);
+        auto ret = fn(std::forward<Args>(args)...);
+        auto value = static_cast<T>(ret);
 
-    class fgcc
+        std::vector<std::any> arg_list = { args... };
+        log<T>(arg_list, name, ret);
+        
+        return ret;
+    }
+
+    template<class U, typename... Args>
+    static void call_original(void* hook, Args&&... args)
     {
-    public:
-        static inline types::fgcc_stun_t original_stun = nullptr;
-        static int __fastcall stun_hook(void* self)
-        {
-            printf("Stun(%p)\n", self);
-            return 0;
-        }
+        auto& [name, ptr] = m_hooks[hook];
+        auto fn = reinterpret_cast<U>(ptr);
+        fn(std::forward<Args>(args)...);
 
-        static inline types::fgcc_get_can_jump_t original_get_can_jump = nullptr;
-        static bool __fastcall get_can_jump_hook(void* self)
+        std::vector<std::any> arg_list = { args... };
+        log(arg_list, name);
+    }
+
+public:
+    template<class T>
+    static bool add_hook(void* base, int offset, void* hook, std::string name)
+    {
+        auto addr = reinterpret_cast<uint64_t>(base) + offset;
+        auto ptr = reinterpret_cast<void*>(addr);
+        m_hooks[hook] = { name, ptr };
+
+        std::cout << "[HOOKS] " << name << " @ " << ptr << std::endl;
+
+        return install_hook(ptr, hook);
+    }
+
+    template<class T>
+    static bool add_hook(void* base, int offset, void* hook)
+    {
+        auto addr = reinterpret_cast<uint64_t>(base) + offset;
+        auto ptr = reinterpret_cast<void*>(addr);
+        m_hooks[hook] = { "", ptr };
+
+        return install_hook(ptr, hook);
+    }
+
+    static bool install_hook(void* ptr, void* hook)
+    {
+        HOOK_TRACE_INFO hHook = { NULL };
+        NTSTATUS result = LhInstallHook(
+            ptr,
+            hook,
+            NULL,
+            &hHook);
+        if (FAILED(result))
         {
-            printf("GetCanJump(%p)\n", self);
+            std::wstring error = RtlGetLastErrorString();
+            return false;
+        }
+        else
+        {
+            ULONG ACLEntries[1] = { 0 };
+            LhSetExclusiveACL(ACLEntries, 1, &hHook);
             return true;
         }
+    }
 
-        static inline types::fgcc_debug_set_zero_gravity_t original_debug_set_zero_gravity = nullptr;
-        static void __fastcall debug_set_zero_gravity_hook(void* self, bool enabled)
+    static int __fastcall set_username_hook(void* self, string* username)
+    {
+        username->value[0] = 'B';
+            
+        return get_original<set_username_t>(set_username_hook)(self, username);
+    }
+
+    static int __fastcall set_nickname_hook(void* self, string* nickname)
+    {
+        nickname->value[0] = 'B';
+            
+        return get_original<set_nickname_t>(set_nickname_hook)(self, nickname);
+    }
+
+    static int __fastcall set_displayname_hook(void* self, string* displayname)
+    {
+        displayname->value[0] = 'B';
+
+        return get_original<set_displayname_t>(set_displayname_hook)(self, displayname);
+    }
+
+    static int __fastcall get_displayname_hook(void* self, net_obj* netobj)
+    {
+        return get_original<get_displayname_t>(get_displayname_hook)(self, netobj);
+    }
+
+    static void* __fastcall debug_make_player_fast_hook(void* self)
+    {
+        printf("fasthook\n");
+        return get_original<debug_make_player_fast_t>(debug_make_player_fast_hook)(self);
+    }
+
+    static void* __fastcall get_my_player_hook(void* self)
+    {
+        return get_original<get_my_player_t>(get_my_player_hook)(self);
+    }
+
+    static int __fastcall fixed_update_all_hook(void* self)
+    {
+        return get_original<fixed_update_all_t>(fixed_update_all_hook)(self);
+    }
+
+    static inline bool hacked = false;
+    static void* __fastcall update_hook(void* self)
+    {
+        if (!hacked)
         {
-            printf("DebugSetZeroGravity(%p, %b)\n", self, enabled);
-            original_debug_set_zero_gravity(self, enabled);
+            if (get_original<get_my_player_t>(get_my_player_hook)(self) != nullptr) // speed
+            {
+                get_original<debug_make_player_fast_t>(debug_make_player_fast_hook)(self);
+                hacked = true;
+            }
         }
-    };
+
+        return get_original<update_t>(update_hook)(self);
+    }
+
+    static void __stdcall log_hook(string* message, void* method)
+    {
+        get_original<log_t>(log_hook)(message, method);
+    }
+
+    static int __fastcall stun_hook(void* self)
+    {
+        printf("Stun(%p)\n", self);
+        return 0;
+    }
+
+    static bool __fastcall get_can_jump_hook(void* self)
+    {
+        printf("GetCanJump(%p)\n", self);
+        return true;
+    }
+
+    static void __fastcall debug_set_zero_gravity_hook(void* self, bool enabled)
+    {
+        get_original<debug_set_zero_gravity_t>(debug_set_zero_gravity_hook)(self, enabled);
+    }
 };
